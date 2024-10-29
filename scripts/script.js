4ced28cfa6c9c113ea5cbd63a3dd037b078df369
Mosiah-A <mosiahassuncao@gmail.com>
2024-10-29 11:25:58 -0300

    fix cep bug

--- scripts/script.js
+++ scripts/script.js
@@ -13,13 +13,19 @@ $(document).ready(function(){
       
     });
   });
-  
+
   function redirectToWhatsApp() {
-    const cep = document.getElementById('cep').value;
+    const cep = document.getElementById('cep').value.trim(); // Remove espaços extras
+
+    if (!cep) { // Verifica se o campo está vazio
+        alert("Por favor, insira o seu CEP."); // Exibe uma mensagem de aviso
+        return false; // Impede o envio e permanece na mesma página
+    }
+
     const encodedMessage = encodeURIComponent(`Olá, gostaria de consultar a disponibilidade para o meu CEP: ${cep}`);
     const whatsappLink = `https://wa.me/558189289155?text=${encodedMessage}`;
 
-    window.open(whatsappLink, '_blank'); // abre em nova aba
-    return false; // impede o envio convencional do formulário
+    window.open(whatsappLink, '_blank'); // Abre em uma nova aba
+    return false; // Impede o envio convencional do formulário
 }
   
